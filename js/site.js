282da4dfa87bf52a41c2b0556ea463216657a3a8
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2022-08-22 11:16:30 -0500

    finished js base code according to SRS

--- js/site.js
+++ js/site.js
@@ -1,49 +1,43 @@
 const marvelHeroes = [
-    "Ant-Man",
-    "Black Panther",
-    "Black Widow",
-    "Captain America",
-    "Doctor Strange",
-    "Hawkeye",
-    "Hulk",
-    "Iron Man",
-    "Luke Cage",
-    "Moon Knight",
-    "Ms. Marvel",
-    "Scarlet Witch",
-    "Spider-Man",
-    "Thor",
-    "Wasp"
+  "Ant-Man",
+  "Black Panther",
+  "Black Widow",
+  "Captain America",
+  "Doctor Strange",
+  "Hawkeye",
+  "Hulk",
+  "Iron Man",
+  "Luke Cage",
+  "Moon Knight",
+  "Ms. Marvel",
+  "Scarlet Witch",
+  "Spider-Man",
+  "Thor",
+  "Wasp"
 ]
 //driver function used for display and passing values.
 function findHero() {
-
+  
     //implement the function findLongestString that returns the longest word.
     let lword = findLongestString(marvelHeroes);
 
     //used for display. 
-    document.getElementById("results").innerHTML = lword;
-
-    
-
-}
+   document.getElementById("results").innerHTML = lword;
+//displays rest of names in the array
+ document.getElementById("nameList").innerHTML = marvelHeroes.join(" | ");
+   
+  }
 
 //takes an array of strings and returns the longest one. 
 function findLongestString(namesArray) {
-   //remove special characters and spaces
-    let regex = /[^a-z0-9]/gi;
-    namesArray = namesArray.replace(regex, "");
-  for (let i = 0; i < namesArray.length; i++) {
-    let newName = namesArray[i];
-    let newHeroesList = [];
-     if (newName.length > lword.length) {
-       lword = newName;
-       newHeroesList.unshift(newName);
-     } else {
-      newHeroesList.shift(newName);
-     }
-  }
-
-    return findLongestString();
-
+  //declare variable
+  let lString = "";
+ 
+  for (let index = 0; index < namesArray.length; index++) {
+    if (namesArray[index].length > lString.length) {
+      lString = namesArray[index];
+      
+    }
+    
+  } return lString;
 }
